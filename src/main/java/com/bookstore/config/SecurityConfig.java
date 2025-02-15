35965bc51da1a5c9dceae25cf7fd73dbff48bbd9
Krishna <krishna.kapoor337@gmail.com>
2025-02-15 21:56:34 +0530

    added some spring security features

---
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+/*
+package com.bookstore.config;
+
+
+//import com.bookstore.service.impl.UserSecurityService;
+//import com.bookstore.utility.SecurityUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class SecurityConfig extends WebSecurityConfigurationAdapter {
+public class SecurityConfig extends WebSecurityConfiguration {
+    @Autowired
+    @Lazy
+    private Environment environment;
+
+    @Autowired
+    @Lazy
+    private UserSecurityService userSecurityService;
+
+    private BCryptPasswordEncoder passwordEncoder(){
+        return SecurityUtility.passwordEncoder();
+    }
+
+    private static final String[] PUBLIC_MATCHER = {
+            "/css/**",
+            "/js/**",
+            "/image/**",
+            "/",
+            "/myAccount",
+    };
+
+    @Override
+    protected void configure(@Lazy HttpSecurity http) throws Exception{
+        http.authorizeRequests().
+                               antMatchers("/**").
+                        requestMatchers(PUBLIC_MATCHER).
+                permitAll().anyRequest().authenticated();
+
+        http.csrf().disable().cors().disable().formLogin().failureUrl("/login?error").loginPage("/login").permitAll().and().logout()
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/?logout").deleteCookies("remember-me")
+                        .permitAll().and().rememberMe();
+    }
+
+//    @Autowired
+//    public void configureGlobal(@Lazy AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+//    }
+
+}
+*/
--- src/main/java/com/bookstore/domain/User.java
+++ src/main/java/com/bookstore/domain/User.java
@@ -2,14 +2,19 @@ package com.bookstore.domain;
 
 
 //import com.bookstore.domain.security.UserRole;
+import com.bookstore.domain.security.Authority;
+import com.bookstore.domain.security.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-//public class User implements UserDetails {
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="id", nullable = false)
@@ -24,8 +29,9 @@ public class User {
     private String phone;
     private boolean enabled=true;
 
-    //@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    //private Set<UserRole> userRoles = new HashSet<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Set<UserRole> userRoles = new HashSet<>();
 
 
     public Long getId() {
@@ -40,31 +46,11 @@ public class User {
         return username;
     }
 
-    /*@Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }*/
 
     public void setUsername(String username) {
         this.username = username;
     }
-
-    /*@Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Set<GrantedAuthority> authorities = new HashSet<>();
-        userRoles.forEach(ur -> authorities.add(new Authority(ur.getRole().getName())));
-        return authorities;
-    }*/
 
     public String getPassword() {
         return password;
@@ -106,20 +92,45 @@ public class User {
         this.phone = phone;
     }
 
-    public boolean isEnabled() {
-        return enabled;
-    }
+
 
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
 
 
-    /*public Set<UserRole> getUserRoles() {
+    public Set<UserRole> getUserRoles() {
         return userRoles;
     }
 
     public void setUserRoles(Set<UserRole> userRoles) {
         this.userRoles = userRoles;
-    }*/
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        userRoles.forEach(ur -> authorities.add(new Authority(ur.getRole().getName())));
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
